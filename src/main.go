5be7db1be4ff99c7431a983f08789304d9bc902b
Nguyen Kim Hiep <nkhiep@tma.com.vn>
2019-05-13 17:52:20 +0700

    update

---
+++ src/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"./channel"
+	"fmt"
+)
+
+func main(){
+	// goroutine
+	//go goroutines.Say("world")
+	//goroutines.Say("hello")
+
+	//channel
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go channel.Sum(s[:len(s)/2], c)
+	go channel.Sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+
+	// buffer
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
